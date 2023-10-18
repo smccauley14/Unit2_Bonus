2105e1433e85ddc84196132b54bd69d731c05392
smccauley841 <smccauley841@gmail.com>
2023-10-18 13:27:51 +0100

    Refactor Bonus feature - Easy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,6 +23,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        checkUserIsInBounds();
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Instantiate(foodPrefab, transform.position, foodPrefab.transform.rotation);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+    }
+
+    void checkUserIsInBounds()
+    {
         if (transform.position.x <= -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -42,15 +57,5 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, yMaxRange);
         }
-
-        if (Input.GetKeyDown(KeyCode.Space)) 
-        {
-            Instantiate(foodPrefab, transform.position, foodPrefab.transform.rotation);
-        }
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-
-        verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
     }
 }
