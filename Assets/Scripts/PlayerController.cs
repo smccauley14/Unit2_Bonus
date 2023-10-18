e380e68b501d82fe4f424e550576c2c7aae8fea9
smccauley841 <smccauley841@gmail.com>
2023-10-18 13:24:27 +0100

    Bonus feature - Easy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Vertical
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
-    private float xRange = 20;
+    private float xRange = 15;
+    private float yMaxRange = 15;
+    private float yMinRange = 0;
 
     public GameObject foodPrefab;
 
@@ -29,11 +33,24 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
-        if(Input.GetKeyDown(KeyCode.Space)) 
+        if (transform.position.z <= yMinRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, yMinRange);
+        }
+
+        if (transform.position.z >= yMaxRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, yMaxRange);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
         {
             Instantiate(foodPrefab, transform.position, foodPrefab.transform.rotation);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    private float spawnRangeX = 20;
+    private float spawnRangeX = 15;
     private float spawnRangeY = 20;
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
